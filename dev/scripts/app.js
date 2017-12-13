594b6e675adc72a5d414cc229ca140b295c85e88
nguyenasaurus <justinexnguyen@gmail.com>
2017-12-12 19:44:23 -0500

    updated layout

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,19 +1,20 @@
 console.log('hey');
 
-  var stickyHeaderTop = $('#stickytypeheader').offset().top;
+  // var stickyHeaderTop = $('#stickytypeheader').offset().top;
 
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > stickyHeaderTop) {
-      $('#stickytypeheader').css({ position: 'fixed', top: '35px' , left: '10%'});
-      $('#sticky').css('display', 'block');
-    } else {
-      $('#stickytypeheader').css({ position: 'static', top: '0px' });
-      $('#sticky').css('display', 'none');
-    }
-  });
+  // $(window).scroll(function () {
+  //   if ($(window).scrollTop() > stickyHeaderTop) {
+  //     $('#stickytypeheader').css({ position: 'fixed', top: '35px' , left: '10%'});
+  //     $('#sticky').css('display', 'block');
+  //   } else {
+  //     $('#stickytypeheader').css({ position: 'static', top: '0px' });
+  //     $('#sticky').css('display', 'none');
+  //   }
+  // });
 
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  pageDots: false
 });

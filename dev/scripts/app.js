cceba9e4501fac51264534f6c3e3000e878a8230
nguyenasaurus <justinexnguyen@gmail.com>
2018-01-12 01:08:32 -0500

    updated contact layout and added blinky chevron to header

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,20 +1,13 @@
 console.log('Thanks for visiting!');
 
-  // var stickyHeaderTop = $('#stickytypeheader').offset().top;
-
-  // $(window).scroll(function () {
-  //   if ($(window).scrollTop() > stickyHeaderTop) {
-  //     $('#stickytypeheader').css({ position: 'fixed', top: '35px' , left: '10%'});
-  //     $('#sticky').css('display', 'block');
-  //   } else {
-  //     $('#stickytypeheader').css({ position: 'static', top: '0px' });
-  //     $('#sticky').css('display', 'none');
-  //   }
-  // });
 
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
   contain: true,
   pageDots: false
+});
+
+$('body').smoothScroll({
+  delegateSelector: 'section.intro button'
 });
